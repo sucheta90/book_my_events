0657a3fefc88ebad214034defe6202fbbc718013
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-01-10 16:46:59 -0500

    fixed code for responsive screen size on Event details

--- src/components/Events/EventDetails.js
+++ src/components/Events/EventDetails.js
@@ -6,17 +6,21 @@ export default function EventDetails(props) {
   return (
     <Card className={styles.details}>
       <img src={props.event.image} className={styles.image} />
-      <div className={styles.main}>
-        <a href={props.event.direction} target="_blank" rel="noreferrer">
-          <p>{`${props.event.location} - ${props.event.city}, ${props.event.state}`}</p>
-        </a>
-        <p>{props.event.description}</p>
-        <div>
-          <button onClick={props.hideDetailHandler}>Go Back</button>
-          <button id="bookNow-btn" onClick={props.bookNowHandler}>
-            Book now
-          </button>
-        </div>
+
+      <a
+        href={props.event.direction}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.link}
+      >
+        <h4>{`${props.event.location} - ${props.event.city}, ${props.event.state}`}</h4>
+      </a>
+      <p className={styles.description}>{props.event.description}</p>
+      <div className={styles.button}>
+        <button onClick={props.hideDetailHandler}>Go Back</button>
+        <button id="bookNow-btn" onClick={props.bookNowHandler}>
+          Book now
+        </button>
       </div>
     </Card>
   );
