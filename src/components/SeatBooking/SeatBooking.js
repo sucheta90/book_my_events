607f874d200456d694bb949c481d0704d15acebb
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-02-20 19:20:48 -0500

    removed unnecessary imports

--- src/components/SeatBooking/SeatBooking.js
+++ src/components/SeatBooking/SeatBooking.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./SeatBooking.module.css";
 import SeatingHeader from "./SeatingHeader";
